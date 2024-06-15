f4d7e7ee9e4cea65c99621f9798b5b1cc8564181
badr dinne el baddi <xanasirox@gmail.com>
2024-06-15 23:37:11 +0100

    ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -5,30 +5,26 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    ft_print_number(int a, int b, int c)
+int ft_print_number(int n1, int n2, int n3)
 {
-    ft_putchar(a);
-    ft_putchar(b);
-    ft_putchar(c);
+    ft_putchar(n1);
+   ft_putchar(n2);
+    ft_putchar(n3);
 
-    if(a < '7')
+    if(n1 < '7')
     {
         ft_putchar(',');
         ft_putchar(' ');
     }
 }
-
-void	ft_print_comb(void)
+void	ft_print_comb2(void)
 {
-    int a;
-    int b;
-    int c;
+    int a, b, c;
 
     a = '0';
-    
     while(a <= '7')
     {
-        b = a + 1;
+        b = a +1;
         while(b <= '8')
         {
             c = b + 1;
