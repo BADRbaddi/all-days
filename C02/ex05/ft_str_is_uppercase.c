90b3903abf3018ee897b7527920a18a1be2a4d2c
badr dinne el baddi <xanasirox@gmail.com>
2024-06-30 21:17:09 +0100

    ex05

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+    int i = 0;
+
+    while(str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            i++;
+        }
+        else
+        return 0;
+    }
+    return 1;
+}
+/*int main()
+{
+    char str[] = "asF";
+    int a = ft_str_is_uppercase(str);
+
+    printf("%d", a);
+}*/
