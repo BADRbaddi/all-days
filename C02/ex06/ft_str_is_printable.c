accdd303612d69fe493cb99975a31f973689bf6d
badr dinne el baddi <xanasirox@gmail.com>
2024-07-02 00:23:41 +0100

    ex06

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<stdio.h>
+
+int	ft_str_is_printable(char *str)
+{
+    int i = 0;
+
+    while(str[i])
+    {
+        if(str[i] >= 32 && str[i] <= 126)
+        {
+            i++;
+        }
+        else
+        return 0;
+    }
+    return 1;
+}
+/*int main()
+{
+    char str[] = "dksgdk$&&*()";
+    int a = ft_str_is_printable(str);
+
+    printf("%d", a);
+}*/
